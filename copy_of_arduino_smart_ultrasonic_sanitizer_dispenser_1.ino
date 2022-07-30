b3ff3ffc954b463538a90a5f2b98dd448ea182c8
Shraddha Hajari <63406385+shraddha7402@users.noreply.github.com>
2022-07-30 20:05:54 +0530

    Add files via upload

---
+++ copy_of_arduino_smart_ultrasonic_sanitizer_dispenser_1.ino
@@ -0,0 +1,44 @@
+#include<Servo.h>
+#define pinServo 2
+Servo S1;
+int degree = 0;
+long cm = 0;
+long readUltrasonicDistance (int triggerPin, int echoPin)
+{
+ pinMode (triggerPin, OUTPUT);
+ digitalWrite (triggerPin, LOW); //sets the trigger pin off
+ delayMicroseconds (2);
+ digitalWrite (triggerPin, HIGH);
+ delayMicroseconds (10);
+ digitalWrite (triggerPin, LOW);
+ pinMode (echoPin, INPUT);//sets the echo pin as input
+ return (pulseIn(echoPin, HIGH)*0.01723);
+}
+void setup ()
+{
+ S1.attach(pinServo);
+ Serial.begin (9600); //initialize serial communication
+ S1.write (0); //set servo to 0 degree
+ delay (2000);
+ pinMode (2, OUTPUT);
+}
+
+void loop ()
+{
+ cm = readUltrasonicDistance ( 5, 4);
+ Serial.print(cm);
+ Serial.println ("cm");
+ if (cm > 180) {
+ digitalWrite (2, LOW);
+ S1.write(0);
+ }
+ if (cm <= 180)
+ {
+ digitalWrite (2, HIGH);
+ S1.write(60);
+ delay (500);
+ S1.write(0);
+ delay (3000);
+ }
+ delay (100);
+}
